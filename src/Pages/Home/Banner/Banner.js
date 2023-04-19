008c0fb7cd9fd89554a8de63769a368536966504
rakibul58 <rhrahi14@gmail.com>
2023-04-19 23:15:31 +0600

    minor changes

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -6,7 +6,7 @@ const Banner = () => {
         <div>
             <div className="hero min-h-screen banner-bottom" style={{ backgroundImage: `url("https://burst.shopifycdn.com/photos/friends-look-on-laptop.jpg?width=925&format=pjpg&exif=1&iptc=1")` }}>
                 <div className="hero-overlay bg-primary bg-opacity-20 banner-bottom"></div>
-                <div className="hero-content text-center mt-64">
+                <div className="hero-content text-center">
                     <div>
                         <h1 className="text-7xl font-bold text-white">How will you <br />
                             change the world?</h1>
